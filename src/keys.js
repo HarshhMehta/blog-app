32e1d2d0c6da0fe6731da01515e871ff97c65869
Harsh Mehta <92149953+HarshhMehta@users.noreply.github.com>
2024-11-30 02:59:47 +0530

    Add files via upload

---
+++ src/keys.js
@@ -0,0 +1,9 @@
+
+
+// keys.js
+export const apiKey = "AIzaSyBykVflTY73XS7V9i4LfbYgeK7t_LkgaOI";
+export const authDomain = "blog-app-4a650.firebaseapp.com";
+export const projectId = "blog-app-4a650";
+export const storageBucket = "blog-app-4a650.firebasestorage.app";
+export const messagingSenderId = "647173387366";
+export const appId = "1:647173387366:web:c730c93e20ce37b58d69a7";
